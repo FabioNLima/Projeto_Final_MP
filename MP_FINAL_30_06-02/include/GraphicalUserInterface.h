4e0a21d231ea7e82544d481d685b9f478c4f676d
Unknown <fabionlima@hotmail.com>
2017-07-03 23:28:29 -0300

    Adicionado o Menu Amizade

--- MP_FINAL_30_06-02/include/GraphicalUserInterface.h
+++ MP_FINAL_30_06-02/include/GraphicalUserInterface.h
@@ -56,38 +56,4 @@ void UI_AdicionarAmigos(Grafo *g, Vertice *aux);
 void UI_Amigos(Grafo *g, Vertice *aux);
 
 
-/**@fn void UI_Transacao()
- * @brief Tela responsável pelo login na rede social
- *@param Recebe uma TAD tipo grafo, para verificar se está cadastrado ou não na rede
- */
-void UI_Transacao(Grafo *g, Vertice *aux);
-
-
-/**@fn void UI_Nova_Transacao()
- * @brief Tela responsável pelo login na rede social
- *@param Recebe uma TAD tipo grafo, para verificar se está cadastrado ou não na rede
- */
-void UI_Nova_Transacao(Grafo *g, Vertice *aux);
-
-
-/**@fn void UI_Busca_Anunucio()
- * @brief Tela responsável pelo login na rede social
- *@param Recebe uma TAD tipo grafo, para verificar se está cadastrado ou não na rede
- */
-void UI_Busca_Anuncio(Grafo *g, Vertice *aux, int valor);
-
-
-/**@fn void UI_Anunuciar()
- * @brief Tela responsável pelo login na rede social
- *@param Recebe uma TAD tipo grafo, para verificar se está cadastrado ou não na rede
- */
-void UI_Anunciar(Grafo *g, Vertice *aux);
-
-/**@fn void UI_Tipo_Produto()
- * @brief Tela responsável pelo login na rede social
- *@param Recebe uma TAD tipo grafo, para verificar se está cadastrado ou não na rede
- */
-int UI_Tipo_Produto(Grafo *g, Vertice *aux);
-
-
 #endif
--- MP_FINAL_30_06-02/include/grafo.h
+++ MP_FINAL_30_06-02/include/grafo.h
@@ -6,6 +6,13 @@
 #include <stdlib.h>
 
 
+typedef struct lista
+{
+	char *informacao;
+	struct lista *prox;
+}Transacao;
+ 
+
 typedef struct vizinhos{
 	struct vertice *p_vertice;
 	struct vizinhos *prox;
@@ -22,9 +29,6 @@ typedef struct vertice{
 	char *login, *nome, *email, *senha;
 	struct vertice *prox;
 	struct aresta *p_arestas;
-	struct anuncio *p_anuncios;
-	struct transacao *p_transacao;
-
 }Vertice;
 
 typedef struct aresta{
@@ -32,24 +36,6 @@ typedef struct aresta{
 	struct aresta *prox;
 	struct vertice *p_vertice;
 }Aresta;
-
-typedef struct anuncio{
-
-	char *tipo;
-	char *titulo;
-	char *descricao;
-	struct anuncio *prox;
-
-}Anuncio;
-
-typedef struct transacao{
-
-	Vertice *login;
-	int avaliacao;
-	struct anuncio *p_anuncios;
-	struct transacao *prox;
-
-}Transacao;
 
 /**
 *   @fn Grafo* cria_grafo(char *)
@@ -71,28 +57,8 @@ int muda_valor_vertice(Grafo *, int, void *);
 void *retorna_valor_aresta(Grafo *, int, int);
 int muda_valor_aresta(Grafo *, int, int, void *);
 
-Anuncio *cria_anuncio();
-
-void destroi_anuncio(Anuncio **);
-
-void adiciona_anuncio(Vertice *, char *, char *, char *);
-
-void retira_anuncio(Anuncio *, char *);
-
-Transacao *cria_transacao();
-
-void destroi_transacao(Transacao **);
-
-void adiciona_transacao(Vertice *, int, Anuncio *);
-
-void retira_transacao(Transacao *, char *);
-
-
 Vertice *verificador_login(Grafo *, char *, char *);
 
 int total_vertice(Grafo *g);
 
 //char **nomes_vertices(Grafo *g, int x);
-
-
-int busca_anuncio_grafo(Grafo *g, char *tipo);
